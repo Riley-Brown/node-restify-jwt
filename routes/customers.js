c44c1ff6b9123979b897099cfd3a7567f3d65148
Riley-Brown <riley@riley.gg>
2019-01-04 23:47:13 -0500

    Added get and post routes for customers and connected to mongoose

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,42 @@
 const errors = require('restify-errors');
-module.exports = server => {
-  server.get('/customers', (req, res, next) => {
-    res.send({
-      msg: 'testing'
-    })
+const Customer = require('../models/Customer');
 
-    next();
-  })
-}
+module.exports = server => {
+
+  /* ===== GET CUSTOMERS ===== */
+  server.get('/customers', async (req, res, next) => {
+    try {
+      const customers = await Customer.find({});
+      res.send(customers);
+      next();
+    } catch (err) {
+      return next(new errors.InvalidContentError(err))
+    }
+  });
+
+  /* ===== ADD CUSTOMER ===== */
+  server.post('/customers', async (req, res, next) => {
+    // check for JSON format
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError("Expects 'application/json' "));
+    }
+    const {
+      name,
+      email,
+      balance
+    } = req.body;
+
+    const customer = new Customer({
+      name,
+      email,
+      balance
+    });
+
+    try {
+      const newCustomer = await customer.save();
+      res.send(201);
+    } catch (err) {
+      return next(new errors.InternalError(err.message));
+    }
+  });
+};
